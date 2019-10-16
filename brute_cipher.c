cb08623574867a29186d95939d73665aea176189
Sarthak Misraa <40424574+sarthakmisraa@users.noreply.github.com>
2019-10-16 22:25:10 +0530

    it broke -_-

--- brute_cipher.c
+++ brute_cipher.c
@@ -13,66 +13,66 @@ int main()
 		c++;	
 	}
 	
-gets(arr);
-printf(" %s", strr);
+fgets(arr,500,stdin);
+puts(arr);
 	for(int j=0;j<26;j++)
 	{
 	for(i=0;i<n;i++)
 	{	switch(arr[i])
 	 {	
-		case 'a' : printf(str[i]);
+		case 'a' : printf("%c",str[i]);
 		break;
-		case 'b' : printf(str[i+1]);
+		case 'b' : printf("%c",str[i+1]);
 		break;
-		case 'c' : printf(str[i+2]);
+		case 'c' : printf("%c",str[i+2]);
 		break;
-		case 'd' : printf(str[i+3]);
+		case 'd' : printf("%c",str[i+3]);
 		break;
-		case 'e' : printf(str[i+4]);
+		case 'e' : printf("%c",str[i+4]);
 		break;
-		case 'f' : printf(str[i+5]);
+		case 'f' : printf("%c",str[i+5]);
 		break;
-		case 'g' : printf(str[i+6]);
+		case 'g' : printf("%c",str[i+6]);
 		break;
-		case 'h' : printf(str[i+7]);
+		case 'h' : printf("%c",str[i+7]);
 		break;
-		case 'i' : printf(str[i+8]);
+		case 'i' : printf("%c",str[i+8]);
 		break;
-		case 'j' : printf(str[i+9]);
+		case 'j' : printf("%c",str[i+9]);
 		break;
-		case 'k' : printf(str[i+10]);
+		case 'k' : printf("%c",str[i+10]);
 		break;
-		case 'l' : printf(str[i+11]);
+		case 'l' : printf("%c",str[i+11]);
 		break;
-		case 'm' : printf(str[i+12]);
+		case 'm' : printf("%c",str[i+12]);
 		break;
-		case 'n' : printf(str[i+13]);
+		case 'n' : printf("%c",str[i+13]);
 		break;
-		case 'o' : printf(str[i+14]);
+		case 'o' : printf("%c",str[i+14]);
 		break;
-		case 'p' : printf(str[i+15]);
+		case 'p' : printf("%c",str[i+15]);
 		break;
-		case 'q' : printf(str[i+16]);
+		case 'q' : printf("%c",str[i+16]);
 		break;
-		case 'r' : printf(str[i+17]);
+		case 'r' : printf("%c",str[i+17]);
 		break;
-		case 's' : printf(str[i+18]);
+		case 's' : printf("%c",str[i+18]);
 		break;
-		case 't' : printf(str[i+19]);
+		case 't' : printf("%c",str[i+19]);
 		break;
-		case 'u' : printf(str[i+20]);
+		case 'u' : printf("%c",str[i+20]);
 		break;
-		case 'v' : printf(str[i+21]);
+		case 'v' : printf("%c",str[i+21]);
 		break;
-		case 'w' : printf(str[i+22]);
+		case 'w' : printf("%c",str[i+22]);
 		break;
-		case 'x' : printf(str[i+23]);
+		case 'x' : printf("%c",str[i+23]);
 		break;
-		case 'y' : printf(str[i+24]);
+		case 'y' : printf("%c",str[i+24]);
 		break;
-		case 'z' : printf(str[i+25]);
+		case 'z' : printf("%c",str[i+25]);
 		break;
-		default :  printf(arr[i]);
+		default :  printf("%c",arr[i]);
 	 }
 	} printf("\n");
 	}
