e1975ece4648127ae1cb1ed10a1405b67248c35f
duckie <40424574+not-duckie@users.noreply.github.com>
2019-12-08 11:06:10 +0530

    added

--- ds/tree.c
+++ ds/tree.c
@@ -3,8 +3,8 @@
 
 struct node {
 	int data;
-	struct tree* left;
-	struct tree* right;
+	struct node* left;
+	struct node* right;
 };
 struct node* root=NULL;
 
@@ -23,10 +23,19 @@ struct node* insert(struct node *root,int data){
 		root->right = insert(root->right,data);
 		}
 		else {
-			root->left = newnode(root->left,data);
+			root->left = insert(root->left,data);
 		}
 	}
 	return root;
+}
+void transverse(struct node* root){
+	if (root==NULL){
+	}
+	else{
+		transverse(root->left);
+		printf("the value %d\n",root->data);
+		transverse(root->right);
+	}
 }
 
 
@@ -38,7 +47,10 @@ int main(){
 	root = insert(root,69);
 	printf("The root node address is %p\n",root);
 	root = insert(root,32);
-	printf("The root node address is %p\n",root);
+	printf("The root node address is %p\n",root->left);
+	root = insert(root,2);
+	printf("The root node address is %p\n",root->left);
+	transverse(root);
 	return 0;
 }
 
