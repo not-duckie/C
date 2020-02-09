6e45cb8bc1cc46621087c7dee6203c2c0e01977d
duckie <sarthakmisraa@gmail.com>
2020-02-09 21:26:28 +0530

    C++ added

---
+++ C++/multiply.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+void multiply(int array1[3][3], int m, int n)
+{ int p,q,arr2[3][3],arr3[3][3],sum=0;
+	
+	cout<<"Enter the number of rows and colums of second matrix\n";
+	cin>>p>>q;
+	
+	if(n!=q) {
+		cout<<"The matix cannot be multiplied\n";
+		exit(1);
+	}
+
+	cout<<"Enter the elements of second matix\n";
+	for(int i =0;i<p;i++)
+	{	for(int j=0;j<q;j++)
+			{ cin>>arr2[i][j];
+			}
+	}
+
+	for(int i=0;i<m;i++){
+		for(int j=0;j<q;j++){
+			for(int k=0;k<p;k++){
+			   sum = sum + (array1[i][k]*arr2[k][j]);
+			}
+		arr3[i][j]=sum;
+		sum=0;}
+	}
+cout<<"the reulting matix is\n";
+	for(int i =0;i<p;i++)
+	{	for(int j=0;j<q;j++)
+			{ cout<<arr3[i][j]<<"\t";
+			}
+			cout<<endl;
+	}
+
+
+}
+
+int main(){
+        int arr[3][3];
+        cout<<"Enter the elements of the matrix "<<endl;
+        for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                        cin>>arr[i][j]; }
+
+        }
+        multiply(arr,3,3);
+				cout<<endl<<endl<<"Aakanksha Datyal"<<endl<<"002 IT/2nd Year"<<endl;
+
+}
+
+
---
+++ C++/transpose.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void transpose(int array1[3][3], int m, int n)
+{ int arr2[3][3];
+
+	
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+		arr2[j][i]=array1[i][j];}
+	}
+
+
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+		printf("%d ", arr2[i][j]);}
+	printf("\n");}
+
+}
+
+int main(){
+	int arr[3][3];
+	cout<<"Enter the elements of the matrix"<<endl;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cin>>arr[i][j];  }
+			
+	}
+	cout<<endl<<endl;
+	transpose(arr,3,3);
+	cout<<endl<<endl<<"Aakanksha Datyal"<<endl<<"002 IT/2nd Year"<<endl;
+}
